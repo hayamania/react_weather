c652892141c1c0da9afa3d5f6cd7822f032987f3
Chie Hym <hayama.chie@gmail.com>
2022-07-02 19:58:44 +0800

    deleted unnecessary file and added footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <p>
+        This project is{" "}
+        <a href="https://github.com/hayamania/react_weather" target={"_blank"}>
+          open-sorced
+        </a>{" "}
+        and coded by Chie HYM
+      </p>
+    </div>
+  );
 }
 
 export default App;
