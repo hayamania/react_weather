68d39d82788840b078fa97bf28a4e315d029c022
Chie Hym <hayama.chie@gmail.com>
2022-07-04 12:40:25 +0800

    Added a grid at current weather

--- src/Weather.js
+++ src/Weather.js
@@ -6,14 +6,30 @@ export default function Weather() {
   return (
     <div className="Weather">
       <div className="Container">
-        <h1>Perth</h1>
-        <p>icon</p>
-        <p>12</p>
-        <p>°C | °F</p>
-        <p>Humidity</p>
-        <p>Wind</p>
-        <p>Sunrise</p>
-        <p>Sunset</p>
+        <div className="row">
+          <form>
+            <input type="search" placeholder="Type a city..." />
+            <input type="submit" value={"Search"} className="btn btn-primary" />
+          </form>
+        </div>
+        <div className="row">
+          <div className="col-sm-12">
+            <h1>Perth</h1>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-sm-6">
+            <p>icon</p>
+            <h2>12</h2>
+            <p>°C | °F</p>
+          </div>
+          <div className="col-sm-6">
+            <p>Humidity</p>
+            <p>Wind</p>
+            <p>Sunrise</p>
+            <p>Sunset</p>
+          </div>
+        </div>
       </div>
     </div>
   );
