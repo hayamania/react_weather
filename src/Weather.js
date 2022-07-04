15496a9fc4926b263c3031e4755862458dd7f306
Chie Hym <hayama.chie@gmail.com>
2022-07-04 10:40:51 +0800

    Imported Bootstrap and created Weather.css

--- src/Weather.js
+++ src/Weather.js
@@ -1,16 +1,20 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Weather.css";
 
 export default function Weather() {
   return (
     <div className="Weather">
-      <h1>Perth</h1>
-      <p>icon</p>
-      <p>12</p>
-      <p>°C | °F</p>
-      <p>Humidity</p>
-      <p>Wind</p>
-      <p>Sunrise</p>
-      <p>Sunset</p>
+      <div className="Container">
+        <h1>Perth</h1>
+        <p>icon</p>
+        <p>12</p>
+        <p>°C | °F</p>
+        <p>Humidity</p>
+        <p>Wind</p>
+        <p>Sunrise</p>
+        <p>Sunset</p>
+      </div>
     </div>
   );
 }
