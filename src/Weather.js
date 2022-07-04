5c14c75fd48703488c185545a9acedfdba51154a
Chie Hym <hayama.chie@gmail.com>
2022-07-04 13:50:27 +0800

    import google fonts

--- src/Weather.js
+++ src/Weather.js
@@ -14,7 +14,7 @@ export default function Weather() {
         <div className="row">
           <form>
             <input type="search" placeholder="Type a city..." />
-            <input type="submit" value={"Search"} className="btn btn-primary" />
+            <input type="submit" value={"Search"} />
           </form>
         </div>
         <div className="row">
@@ -23,12 +23,14 @@ export default function Weather() {
           </div>
         </div>
         <div className="row">
-          <div className="col-sm-6">
+          <div className="col-sm-4">
             <WiDaySunny size={70} className="icons" />
+          </div>
+          <div className="col-sm-4">
             <h2>12</h2>
             <p>°C | °F</p>
           </div>
-          <div className="col-sm-6">
+          <div className="col-sm-4">
             <p>
               <WiHumidity size={24} className="icons" />
               Humidity
