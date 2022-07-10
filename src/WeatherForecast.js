6be9defe0f21733701433c8f84c1244d7e207a18
Chie Hym <hayama.chie@gmail.com>
2022-07-10 20:18:06 +0800

    changed forecast data from dummy to real info from API

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,40 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import "./WeatherForecast.css";
 import WeatherIcon from "./WeatherIcon";
+import FormatDate from "./FormatDate";
 import axios from "axios";
 import { WiSunrise, WiSunset } from "weather-icons-react";
 
 export default function WeatherForecast(props) {
-  const apiKey = `a5819625e2717720981216aa54bee886`;
-  const apiUnits = `metric`;
-  const apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${props.coordinates.lat}&lon=${props.coordinates.lon}&exclude=current,minutely,hourly,alerts&appid=${apiKey}&units=${apiUnits}`;
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
 
-  function getForecast(response) {
-    console.log(response);
-  }
-
-  axios.get(apiUrl).then(getForecast);
-
-  return (
-    <div className="WeatherForecast">
-      <div className="container">
-        <div className="row">
-          <div className="col">
-            <h1>Mon</h1>
-            <WeatherIcon code={"02d"} size={50} />
-            <div className="temperatures mb-1">
-              <span className="temperature-max">27°</span>{" "}
-              <span className="temperature-min">15°</span>
-            </div>
-            <div className="sunrise">
-              <WiSunrise size={20} className="icons" /> 06:24
-            </div>
-            <div className="sunset">
-              <WiSunset size={20} className="icons" /> 19:12
+  if (loaded) {
+    return (
+      <div className="WeatherForecast">
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <h1>
+                <FormatDate date={forecast.day} />
+              </h1>
+              <WeatherIcon code={"02d"} size={50} />
+              <div className="temperatures mb-1">
+                <span className="temperature-max">{forecast.temp_max}°</span>{" "}
+                <span className="temperature-min">{forecast.temp_min}°</span>
+              </div>
+              <div className="sunrise">
+                <WiSunrise size={20} className="icons" /> {forecast.sunrise}
+              </div>
+              <div className="sunset">
+                <WiSunset size={20} className="icons" /> {forecast.sunset}
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = `a5819625e2717720981216aa54bee886`;
+    const apiUnits = `metric`;
+    const apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${props.coordinates.lat}&lon=${props.coordinates.lon}&exclude=current,minutely,hourly,alerts&appid=${apiKey}&units=${apiUnits}`;
+    function getForecast(response) {
+      console.log(response.data.daily[0].sunrise);
+      console.log(new Date(response.data.daily[0].dt * 1000).getDay());
+      setForecast({
+        day: new Date(response.data.daily[0].dt * 1000).getDay(),
+        temp_max: Math.round(response.data.daily[0].temp.max),
+        temp_min: Math.round(response.data.daily[0].temp.min),
+        sunrise: `${String(
+          new Date(response.data.daily[0].sunrise * 1000).getHours()
+        ).padStart(2, `0`)}:${String(
+          new Date(response.data.daily[0].sunrise * 1000).getMinutes()
+        ).padStart(2, `0`)}`,
+        sunset: `${String(
+          new Date(response.data.daily[0].sunset * 1000).getHours()
+        ).padStart(2, `0`)}:${String(
+          new Date(response.data.daily[0].sunset * 1000).getMinutes()
+        ).padStart(2, `0`)}`,
+      });
+      setLoaded(true);
+    }
+    axios.get(apiUrl).then(getForecast);
+    return null;
+  }
 }
