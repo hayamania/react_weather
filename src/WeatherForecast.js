b46f3b586a5a4b09eaa2f3e0feb651d0d10088bd
Chie Hym <hayama.chie@gmail.com>
2022-07-10 21:57:25 +0800

    changed to set forecast all daily data

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -7,13 +7,18 @@ export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
 
+  function getForecast(response) {
+    setForecast(response.data.daily);
+    setLoaded(true);
+  }
+
   if (loaded) {
     return (
       <div className="WeatherForecast">
         <div className="container">
           <div className="row">
             <div className="col">
-              <WeatherForecastDay forecast={forecast} />
+              <WeatherForecastDay forecast={forecast[0]} />
             </div>
           </div>
         </div>
@@ -23,10 +28,6 @@ export default function WeatherForecast(props) {
     const apiKey = `a5819625e2717720981216aa54bee886`;
     const apiUnits = `metric`;
     const apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${props.coordinates.lat}&lon=${props.coordinates.lon}&exclude=current,minutely,hourly,alerts&appid=${apiKey}&units=${apiUnits}`;
-    function getForecast(response) {
-      setForecast(response.data.daily[0]);
-      setLoaded(true);
-    }
     axios.get(apiUrl).then(getForecast);
     return null;
   }
