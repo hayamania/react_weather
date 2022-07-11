21afbd82b93d876042c69770ac4274976a75228d
Chie Hym <hayama.chie@gmail.com>
2022-07-11 09:56:57 +0800

    deleted unused part

--- src/Weather.js
+++ src/Weather.js
@@ -1,4 +1,4 @@
-import React, { useState, CSSProperties } from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./Weather.css";
